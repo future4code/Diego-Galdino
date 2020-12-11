1f94a489c9aaf0a488073f0a9d42aa535f9b6cbf
Diego Galdino <diegogaldino1345@hotmail.com>
2020-12-11 12:47:03 -0300

    Exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,24 @@
+// Exercícios de interpretação de código
+
+// 1 - A função conversorDeMoeda espera receber como parametro um valor, 
+// onde dentro da função a variavel cotação recebe do usuario o valor da cotação do dolar.
+// E retorna a multiplicação da cotacao com o valor recebido via parametro e concatenando o R$
+// é criada uma const que recebe o retorno da função conversorDeMoeda e logo apos é exibida no console.log 
+// dolar 100 cotação 5,08 console.log 507,78
+
+// 2 - A função investeDinheiro espera receber como parametro o tipoDeinvestimento e um valor
+// cria uma variavel valorAposInvestimento
+// Escolhe um investimento atraves do switch passando a variavel tipoDeinvestimento
+// E retornando o valorAposInvestimento recebendo a multiplicação de valor com uma porcentagem
+// e se o tipoDeinvestimento não for nenhum dos esperados retorna investimento incorreto
+// é criado 2 const que recebe o resultado da investeDinheiro e exibido os resultados
+// que são novoMontante 165 segundoMontante "Tipo de investimento informado incorreto"
+
+// 3 - é criado 3 arrays numeros array1 e array2 onde numeros ja contem valores
+// dentro de uma estrutura de repetição for é separado os numeros entre pares e impares do array numeros
+// e armazendo os pares em array1 e impares no array2
+// no final exibe o total de numeros do array numeros, depois o total de pares e impares 
+
+// 4 - é criado 1 array numeros e duas variaveis numero1 e numero2 todos com valores iniciados
+// executa um for of verificando se o item do array numeros é maior ou menor e armazenando em numero1 e numero2 respectivamente
+// logo apos exibindo menor -10 e maior 1590
