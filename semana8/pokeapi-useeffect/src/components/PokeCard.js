80f2b40b3545b93d9efe33266df2b72e5a84d618
Diego Galdino <diegogaldino1345@hotmail.com>
2021-01-26 15:04:21 -0300

    pokeApiUseEffect

---
+++ semana8/pokeapi-useeffect/src/components/PokeCard.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function PokeCard(props) {
+    const [pokemon, setPokemon] = useState({})
+    console.log("use 3",pokemon)
+    useEffect(
+        () => {
+            console.log("use 1", props.pokemon)
+            pegaPokemon(props.pokemon)
+        }, [])
+
+    useEffect(() => {
+        if (pokemon.name !== props.pokemon) {
+            pegaPokemon(props.pokemon)
+        }
+    }, [props.pokemon])
+
+    const pegaPokemon = pokeName => {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+            .then(response => {
+                console.log("use 1", pokemon)
+                setPokemon(response.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <div>
+            <p>{pokemon.name}</p>
+            <p>{pokemon.weight} Kg</p>
+            {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+            {pokemon.sprites && (
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            )}
+        </div>
+    )
+}
