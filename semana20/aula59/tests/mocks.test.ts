b9342a665455f980b557c0d9bfeb1f1da4649810
Diego Galdino <diegogaldino1345@hotmail.com>
2021-04-20 16:14:15 -0300

    jest & mocks

---
+++ semana20/aula59/tests/mocks.test.ts
@@ -0,0 +1,68 @@
+import { Character, performAttack2 } from "../src/character";
+
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return true
+		});
+});
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return false
+		});
+});
+
+test("Should perform attack", () => {
+    const validatorMock = jest.fn(() => {
+      return true;
+    });
+
+    const attacker: Character = {
+      name: "Scorpion",
+      life: 1500,
+      defense: 200,
+      strength: 600,
+    };
+
+    const defender: Character = {
+      name: "Kitana",
+      life: 1500,
+      defense: 400,
+      strength: 800,
+    };
+
+    performAttack2(attacker, defender, validatorMock as any);
+
+    expect(defender.life).toEqual(1300);
+    expect(validatorMock).toHaveBeenCalled();
+    expect(validatorMock).toHaveBeenCalledTimes(2);
+    expect(validatorMock).toHaveReturnedTimes(2);
+  });
+  test("Should return invalid character error", () => {
+    expect.assertions(4);
+    const validatorMock = jest.fn(() => {
+      return false;
+    });
+
+    const attacker: Character = {
+      name: "Scorpion",
+      life: 1500,
+      defense: 200,
+      strength: 600,
+    };
+
+    const defender: Character = {
+      name: "Kitana",
+      life: 1500,
+      defense: 400,
+      strength: 800,
+    };
+
+    try {
+      performAttack2(attacker, defender, validatorMock as any);
+    } catch (err) {
+      expect(err.message).toBe("Invalid character");
+      expect(validatorMock).toHaveBeenCalled();
+      expect(validatorMock).toHaveBeenCalledTimes(1);
+      expect(validatorMock).toHaveReturnedTimes(1);
+    }
+  });
